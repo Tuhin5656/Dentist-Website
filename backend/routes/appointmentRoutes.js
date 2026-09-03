7b33d224607d94708636a0d75b098df01338d336
Tuhin5656 <tuhinmallick58@gmail.com>
2026-09-03 22:48:13 +0530

    Update appointmentRoutes.js

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const router = express.Router();
-const { createAppointment } = require('../controllers/appointmentController');
 
-router.post('/appointments', createAppointment);
+const router = express.Router();
+
+const {
+    createAppointment
+} = require('../controllers/appointmentController');
+
+
+// POST Appointment
+router.post(
+    '/appointments',
+    createAppointment
+);
+
 
 module.exports = router;
